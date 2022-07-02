05cfbf25c97299abf5eca18626d03dda48b0721b
luisangeluis <luisangeluis@gmail.com>
2022-07-02 18:00:45 -0500

    Se configura motor de plantillas hbs o handlebars
    Se configura el motor de plantillas handlebars el main layout y el uso
    de partials.
    Todo esto para manejar html de forma dinamica.

--- src/app.js
+++ src/app.js
@@ -1,10 +1,23 @@
 import express from "express";
-import  indexRoutes from './routes/index.routes'
+import exphbs, { create } from "express-handlebars";
+import indexRoutes from "./routes/index.routes";
+import path from "path";
 
 //Se configura el server para que funcione y pueda usar cada de las rutas en este caso de indes
-
 const app = express();
 
+//Se configura para usar las views con express handlebars
+app.set("views", path.join(__dirname, "views"));
+const hbs = create({
+  layoutsDir: path.join(app.get("views"), "layouts"),
+  partialsDir:path.join(app.get('views'),'partials'),
+  defaultLayout: "main",
+  extname: ".hbs",
+});
+app.engine(".hbs",hbs.engine);
+app.set("view engine", ".hbs");
+
+//Se agregan todas las rutas
 app.use(indexRoutes);
 
 export default app;
--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,15 +1,23 @@
-import {Router} from 'express';
+import { Router } from "express";
 
-const router =Router();
+const router = Router();
 
 //Configuracion de cada una de las rutas en este caso del index
 
+// router.get("/", (req, res) => {
+//   res.send("<h1>hello world</h1>");
+// });
+
 router.get("/", (req, res) => {
-  res.send("<h1>hello world</h1>");
+  res.render("index");
 });
 
 router.get("/about", (req, res) => {
-    res.send("about");
-  });
+  res.render("about");
+});
+
+router.get("/edit", (req, res) => {
+  res.render("edit");
+});
 
 export default router;
