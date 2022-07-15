56b6416ab1d0fcbdfd5e641ef2a8eee21933bca9
luisangeluis <luisangeluis@gmail.com>
2022-07-14 21:56:01 -0500

    Formulario y metodo post
    Se crea formulario y se configura para enviar datos por post.

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ import express from "express";
 import exphbs, { create } from "express-handlebars";
 import indexRoutes from "./routes/index.routes";
 import path from "path";
+//MORGAN
+import morgan from 'morgan';
 
 //Se configura el server para que funcione y pueda usar cada de las rutas en este caso de indes
 const app = express();
@@ -17,6 +19,12 @@ const hbs = create({
 app.engine(".hbs",hbs.engine);
 app.set("view engine", ".hbs");
 
+
+//MORGAN
+//Sirve para ver las peticiones get y post por consola
+app.use(morgan('dev'))
+//Para recibir como json la info del post
+app.use(express.urlencoded({extended:false}))
 //Se agregan todas las rutas
 app.use(indexRoutes);
 
--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-
+import Task from '../models/Task';
 const router = Router();
 
 //Configuracion de cada una de las rutas en este caso del index
@@ -19,5 +19,13 @@ router.get("/about", (req, res) => {
 router.get("/edit", (req, res) => {
   res.render("edit");
 });
+
+router.post('/tasks/add',(req,res)=>{
+  console.log(req.body);
+  const task =Task(req.body)
+  console.log(task);
+
+  res.send('added');
+})
 
 export default router;
